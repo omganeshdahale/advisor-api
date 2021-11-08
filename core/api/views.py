2b9f4fdaa3acb0286d024fb3e36d1a5eacd478c7
Om Dahale <om.g.dahale@gmail.com>
2021-11-08 10:39:45 +0530

    validate time in BookingCreateView

--- core/api/views.py
+++ core/api/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-from rest_framework import generics
+from django.utils.dateparse import parse_datetime
+from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -29,7 +30,21 @@ class BookingCreateView(APIView):
     def post(self, request, user_pk, advisor_pk, format=None):
         user = get_object_or_404(User, pk=user_pk)
         advisor = get_object_or_404(Advisor, pk=advisor_pk)
-        Booking.objects.create(user=user, advisor=advisor, time=request.data["time"])
+        time = request.data.get("time")
+        if time is None:
+            return Response(
+                {"time": ["This field is required."]},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        time = parse_datetime(time)
+        if time is None:
+            return Response(
+                {"time": ["Enter a valid datetime, use format yyyy-mm-dd hh:mm"]},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        Booking.objects.create(user=user, advisor=advisor, time=time)
         return Response()
 
 
